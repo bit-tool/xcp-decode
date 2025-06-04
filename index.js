f8742150d8d2e87e4e6b17ebbe3f780afc499042
TS <tian.s@bitgc.io>
2025-06-04 20:15:27 +0800

    Format display info

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 import { decodeTransaction } from './btc.js';
 import rc4 from './rc4.js';
 import * as bitcoin from 'bitcoinjs-lib';
+import chalk from 'chalk';
 
 function decodeAddress(address) {
     const version = address.readUInt8(0);
@@ -17,6 +18,7 @@ function decodeAddress(address) {
 function decodeCounterpartyTransaction(tx) {
     // Counterparty transactions are obfuscated with RC4 using the first input's txid as the key
     const key = tx.inputs[0].txid;
+    const counterparty = [];
 
     tx.outputs.forEach((output, index) => {
         if (output.script.type !== 'OP_RETURN') {
@@ -40,7 +42,19 @@ function decodeCounterpartyTransaction(tx) {
             amount: data.readBigInt64BE(8).toString(),
             memo: data.subarray(37).toString('utf-8'),
         }
+        counterparty.push(output.script.counterparty);
     });
+    return counterparty;
+}
+
+function formatAmount(amount) {
+    // Parse amount to BigInt, divide by 100,000,000, and display with decimal points
+    const amountBigInt = BigInt(amount);
+    const divisor = 100000000n;
+    const whole = amountBigInt / divisor;
+    const fraction = amountBigInt % divisor;
+    const fractionStr = fraction.toString().padStart(8, '0');
+    return `${whole.toString()}.${fractionStr}`;
 }
 
 // Get the transaction hex from command line arguments
@@ -54,9 +68,19 @@ if (!txHex) {
 
 try {
     const tx = decodeTransaction(txHex);
-    decodeCounterpartyTransaction(tx);
-    console.log(JSON.stringify(tx, null, 2));
+    const counterparty = decodeCounterpartyTransaction(tx);
+    console.log(chalk.gray(JSON.stringify(tx, null, 2)));
+
+    counterparty.forEach(c => {
+        console.log(chalk.gray('--------------------------------'));
+        console.log(`Counterparty: ${c.type}`);
+        console.log(`Asset: ${c.asset === '1' ? 'XCP' : c.asset}`);
+        console.log('From: ' + (c.from? chalk.blue(c.from): chalk.gray('(Unsigned TX)')));
+        console.log('To: ' + chalk.blue(c.to));
+        console.log('Amount: ' + chalk.blue(formatAmount(c.amount)));
+        console.log(chalk.gray('--------------------------------'));
+    });
 } catch (error) {
-    console.error(error.message);
+    console.error(chalk.red(`Error: ${error.message}`));
     process.exit(1);
 }
